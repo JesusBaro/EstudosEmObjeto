15878e97fdf72185d619fa1929d865715f94fbc5
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-01-31 02:50:04 -0300

    feito até o 06

---
+++ script.js
@@ -0,0 +1,42 @@
+// 01 - Crie um array que receba 5 itens e exiba no console.
+
+// let estilosMusicais = [`jazz`,`samba`,`funk`,`lo-fi`,`alternativo`,]
+// console.log(estilosMusicais)
+
+// 02 - Utilize um método para adicionar um nome ao inicio do array.
+
+// estilosMusicais.splice(0,0,`Disco`)
+// console.log(estilosMusicais)
+
+// 03 - Utilize um método para remover o último nome do array.
+
+// estilosMusicais.splice(4,1)
+// console.log(estilosMusicais)
+
+// 04 - Utilize um método para adicionar dois nomes ao fim do array.
+
+// estilosMusicais.splice(5,0,`R&B`)
+// console.log(estilosMusicais)
+
+// 05 - Utilize um método para remover o primeiro nome do array.
+
+// estilosMusicais.shift()
+// console.log(estilosMusicais)
+
+// 06 - Utilize um método para organizar em ordem crescente o seguinte array:
+// let numbers = [7,5,6,3,8,9,2,1,4]
+// numbers.sort((a,b) => a-b)
+// console.log(numbers)
+// 07 - Crie um objeto que receba ao menos três propriedades sobre você.
+
+// 08 - Adicione uma nova propriedade sem alterar seu objeto inicial.
+
+// 09 - Remova uma propriedade desse objeto.
+
+// 10 - Mostre no console todas as propriedades desse objeto.
+
+// 11- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+// Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+// Na propriedade amigos, adicione ao menos 4 itens.
+
+// 12 - Mostre no console o nome de um amigo de cada lista.
