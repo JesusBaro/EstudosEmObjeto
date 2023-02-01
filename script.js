86e3d5f442932282076f001c0b8816ba6da608e7
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-02-01 00:42:01 -0300

    pronto para enviar

--- script.js
+++ script.js
@@ -1,65 +1,55 @@
 // 01 - Crie um array que receba 5 itens e exiba no console.
 
-// let estilosMusicais = [`jazz`,`samba`,`funk`,`lo-fi`,`alternativo`,]
-// console.log(estilosMusicais)
+let estilosMusicais = [`jazz`,`samba`,`funk`,`lo-fi`,`alternativo`,]
 
 // 02 - Utilize um método para adicionar um nome ao inicio do array.
 
-// estilosMusicais.splice(0,0,`Disco`)
-// console.log(estilosMusicais)
+estilosMusicais.splice(0,0,`Disco`)
 
 // 03 - Utilize um método para remover o último nome do array.
 
-// estilosMusicais.splice(4,1)
-// console.log(estilosMusicais)
+estilosMusicais.splice(4,1)
 
 // 04 - Utilize um método para adicionar dois nomes ao fim do array.
 
-// estilosMusicais.splice(5,0,`R&B`)
-// console.log(estilosMusicais)
+estilosMusicais.splice(5,0,`R&B`)
 
 // 05 - Utilize um método para remover o primeiro nome do array.
 
-// estilosMusicais.shift()
-// console.log(estilosMusicais)
+estilosMusicais.shift()
 
 // 06 - Utilize um método para organizar em ordem crescente o seguinte array:
 
-// let numbers = [7,5,6,3,8,9,2,1,4]
-// numbers.sort((a,b) => a-b)
-// console.log(numbers)
+let numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort((a,b) => a-b)
 
 // 07 - Crie um objeto que receba ao menos três propriedades sobre você.
 
-// let jesus={
-//     etnia:`negro`,
-//     naturalidade:`Rio de Janeiro`,
-//     idade:24,
-// }
-// console.log(jesus)
+let humano={
+    nome:`Jésus`,
+    etnia:`negro`,
+    naturalidade:`Rio de Janeiro`,
+    idade:24,
+}
 
 // 08 - Adicione uma nova propriedade sem alterar seu objeto inicial.
 
-// jesus.altura = 1.83
-// console.log(jesus)
+humano.altura = 1.83
 
 // 09 - Remova uma propriedade desse objeto.
 
-// delete jesus.naturalidade
-// console.log(jesus)
+// delete humano.naturalidade
 
 // 10 - Mostre no console todas as propriedades desse objeto.
 
-// console.log(jesus.etnia)
-// console.log(jesus.naturalidade)
-// console.log(jesus.idade)
-
+console.log(humano.nome)
+console.log(humano.etnia)
+console.log(humano.naturalidade)
+console.log(humano.idade)
 
 // 11- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
 // Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
 // Na propriedade amigos, adicione ao menos 4 itens. 
-
-// - amigos é uma array dentro de um objeto //
 
 let cadastro = [
     {
