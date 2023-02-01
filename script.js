fbcf55c48bd07c3244acce91c6ed9c72fa4f19c3
Jésus Baro <118184872+JesusBaro@users.noreply.github.com>
2023-01-31 23:15:48 -0300

    completo

--- script.js
+++ script.js
@@ -24,19 +24,89 @@
 // console.log(estilosMusicais)
 
 // 06 - Utilize um método para organizar em ordem crescente o seguinte array:
+
 // let numbers = [7,5,6,3,8,9,2,1,4]
 // numbers.sort((a,b) => a-b)
 // console.log(numbers)
+
 // 07 - Crie um objeto que receba ao menos três propriedades sobre você.
+
+// let jesus={
+//     etnia:`negro`,
+//     naturalidade:`Rio de Janeiro`,
+//     idade:24,
+// }
+// console.log(jesus)
 
 // 08 - Adicione uma nova propriedade sem alterar seu objeto inicial.
 
+// jesus.altura = 1.83
+// console.log(jesus)
+
 // 09 - Remova uma propriedade desse objeto.
+
+// delete jesus.naturalidade
+// console.log(jesus)
 
 // 10 - Mostre no console todas as propriedades desse objeto.
 
+// console.log(jesus.etnia)
+// console.log(jesus.naturalidade)
+// console.log(jesus.idade)
+
+
 // 11- Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
 // Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
-// Na propriedade amigos, adicione ao menos 4 itens.
+// Na propriedade amigos, adicione ao menos 4 itens. 
+
+// - amigos é uma array dentro de um objeto //
+
+let cadastro = [
+    {
+        nome:`Lara`,
+        idade:20,
+        telefone:9885285110,
+        amigos:[`Dara`,
+                 `Carla`, 
+                 `Ana Paula`, 
+                 `Ajay`
+                ]
+    },
+    { 
+        nome:`Miguel`,
+        idade:27,
+        telefone:947365739,
+        amigos:[`Danilo`,
+            `Fernanda`,
+            `Josy`,
+            `Rómulo`,
+            ]
+        },
+    { 
+        nome:`Alessandro`,
+        idade:26,
+        telefone:943838548,
+        amigos:[`Gabriel`,
+            `Regina`,
+            `Kelly`,
+            `Mônica`,
+            ]
+        },
+    { 
+        nome:`Jojo`,
+        idade:22,
+        telefone:null,
+        amigos:[`Sarah`,
+            `Gustavo`,
+            `Silva`,
+            `Douglas`,
+            ]
+        },
+]
 
 // 12 - Mostre no console o nome de um amigo de cada lista.
+
+console.log(cadastro[0].nome)
+console.log(cadastro[1].nome)
+console.log(cadastro[2].nome)
+console.log(cadastro[3].nome)
